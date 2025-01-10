e2cd1394a8dd73ecf36899c40f5c0e05d5abdcc3
kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
2025-01-10 18:44:57 +0700

    Day 2: Testing, GO có hỗ trợ xem độ bao phủ của code, lên Notion xem để thấy rõ hơn

--- test/basic/main_test.go
+++ test/basic/main_test.go
@@ -1,11 +1,7 @@
 package basic
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 // để biết 1 file test nào thì phải có tên file test là tên file cần test + _test.go
@@ -18,23 +14,5 @@ func TestAddONe(t *testing.T) {
 	if actual != outputAddOne {
 		t.Errorf("AddOne(%d) = %d; want %d", inputAddOne, actual, outputAddOne)
 	}
-
-}
-
-func TestRequire(t *testing.T) {
-	fmt.Println("Test 2 test case")
-	require.Equal(t, 2, AddOne(1))
-	fmt.Println("AddOne test 1 passed")
-	require.Equal(t, 2, AddOne(2))
-	fmt.Println("AddOne test 2 passed")
-}
-
-func TestAssert(t *testing.T) {
-	fmt.Println("Test 2 test case")
-	assert := assert.New(t)
-	assert.Equal(t, 2, AddOne(1))
-	fmt.Println("AddOne test 1")
-	assert.Equal(t, 2, AddOne(2))
-	fmt.Println("AddOne test 2")
 
 }
