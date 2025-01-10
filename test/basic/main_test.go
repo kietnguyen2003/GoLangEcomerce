ffa812a32daf314e6f7a1bb970bf1581efdcfc4b
kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
2025-01-10 18:26:14 +0700

    Day 2: Unit test, basic Testing

---
+++ test/basic/main_test.go
@@ -0,0 +1,16 @@
+package basic
+
+import "testing"
+
+// để biết 1 file test nào thì phải có tên file test là tên file cần test + _test.go
+
+func TestAddONe(t *testing.T) {
+	inputAddOne := 1
+	outputAddOne := 2
+
+	actual := AddOne(inputAddOne)
+	if actual != outputAddOne {
+		t.Errorf("AddOne(%d) = %d; want %d", inputAddOne, actual, outputAddOne)
+	}
+
+}
